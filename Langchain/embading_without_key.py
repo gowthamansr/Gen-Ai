4b9d94297af3a4c9d88c17697ce40aa5c0aaad4f
gowthamansr <46374351+gowthamansr@users.noreply.github.com>
2026-05-23 11:46:12 +0530

    Update
    Update

--- Langchain/embading_without_key.py
+++ Langchain/embading_without_key.py
@@ -2,5 +2,5 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 hugging_face_embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 text_embed = "This is one of the best Gen AI courses I have ever taken. I highly recommend it to anyone interested in learning about GenAI."
-embedding = hugging_face_embeddings.embed_query(text_embed)
-print(embedding[:5])
+hugging_face_embedd = hugging_face_embeddings.embed_query(text_embed)
+print(hugging_face_embedd[:5])
